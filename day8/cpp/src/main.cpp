bd01da68ea3caacd89a117a127fa3fb537d367da
Marc Chapdelaine <marc.chapdelaine@gmail.com>
2020-12-08 22:03:44 -0500

    Day 8 : Complete

--- day8/cpp/src/main.cpp
+++ day8/cpp/src/main.cpp
@@ -8,14 +8,13 @@
 
 using namespace std;
 
-int executeOnce(const vector<InstructionClass>& pInstructions)
+bool executeOnce(const vector<InstructionClass>& pInstructions, int& pAcc)
 {
     unsigned int lProgramCounter = 0;
-    int lAcc = 0;
-
+    pAcc = 0;
     unsigned short lExecutionCount[pInstructions.size()] = { 0 };
 
-    while (true)
+    while (lProgramCounter < pInstructions.size())
     {
         if (lExecutionCount[lProgramCounter] == 1)
         {
@@ -30,7 +29,7 @@ int executeOnce(const vector<InstructionClass>& pInstructions)
         }
         else if (pInstructions[lProgramCounter].getInstuction() == InstructionClass::InstructionEnum::ACC)
         {
-            lAcc += pInstructions[lProgramCounter].getValue();
+            pAcc += pInstructions[lProgramCounter].getValue();
             lProgramCounter++;
         }
         else if (pInstructions[lProgramCounter].getInstuction() == InstructionClass::InstructionEnum::JMP)
@@ -43,13 +42,47 @@ int executeOnce(const vector<InstructionClass>& pInstructions)
         }
     }
 
-    return lAcc;
+    return lProgramCounter == pInstructions.size();
 }
 
+bool modifyProgram(const vector<InstructionClass>& pInstructions, int& pAcc)
+{
+    // Try to loop and change each JMP and NOP to see what happen
+
+    for (unsigned int lCurrentTry = 0; lCurrentTry < pInstructions.size(); lCurrentTry++)
+    {
+        if (pInstructions[lCurrentTry].getInstuction() == InstructionClass::InstructionEnum::ACC)
+        {
+            continue;
+        }
+        else if (pInstructions[lCurrentTry].getInstuction() == InstructionClass::InstructionEnum::JMP)
+        {
+            vector<InstructionClass> lNewInstructions = pInstructions;
+            lNewInstructions[lCurrentTry].setInstruction(InstructionClass::InstructionEnum::NOP);
+            if (executeOnce(lNewInstructions, pAcc))
+            {
+                cout << "Gotcha (JMP to NOP) at line " << lCurrentTry << endl;
+                return true;
+            }
+        }
+        else if (pInstructions[lCurrentTry].getInstuction() == InstructionClass::InstructionEnum::NOP)
+        {
+            vector<InstructionClass> lNewInstructions = pInstructions;
+            lNewInstructions[lCurrentTry].setInstruction(InstructionClass::InstructionEnum::JMP);
+            if (executeOnce(lNewInstructions, pAcc))
+            {
+                cout << "Gotcha (NOP to JMP) at line " << lCurrentTry << endl;
+                return true;
+            }
+        }
+    }
+
+    return false;
+}
 
 int main(int pArgc, char** pArgv) 
 {
-    cout << "Day 3\n";
+    cout << "Day 8\n";
     
     if (pArgc < 2)
     {
@@ -73,7 +106,22 @@ int main(int pArgc, char** pArgv)
         lInstructions.emplace_back(InstructionClass(lTemp));
     }
 
-    cout << "For part 1, the accumulator is " << executeOnce(lInstructions) << endl;
+    
+    int lAcc = 0;
+    executeOnce(lInstructions, lAcc);
+    cout << "For part 1, the accumulator is " << lAcc << endl;
+
+    cout << "Find line to change " << endl;
+    if (modifyProgram(lInstructions, lAcc))
+    {
+        cout << "Complete " << endl;
+    }
+    else
+    {
+        cout << "No solution" << endl;
+    }
+    
+    cout << "Accumulator is " << lAcc << endl;
 
     return EXIT_SUCCESS;
 }
