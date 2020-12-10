17dc63df00080c42c759cadc706198daca2174c5
Marc Chapdelaine <marc.chapdelaine@gmail.com>
2020-12-10 05:19:02 -0500

    Day 9 : Complete

--- day9/cpp/src/main.cpp
+++ day9/cpp/src/main.cpp
@@ -49,15 +49,69 @@ uint64_t findInvalidValue(vector<uint64_t> pValues, unsigned int pFirstNumbers)
     return 0;
 }
 
+uint64_t resultFindRange(vector<uint64_t> pValues, uint64_t pValueToSearch)
+{
+    // Erase the value we search and all values that are over the limit
+    vector<uint64_t> lValues;
+    for (const auto lValue : pValues)
+    {
+        if (lValue < pValueToSearch)
+        {
+            lValues.emplace_back(lValue);
+        }
+    }
+
+    if (lValues.size() < 2)
+    {
+        throw std::runtime_error("Invalid number of entries");
+    }
+
+    for (unsigned int lNumberNumbers = 2; lNumberNumbers < lValues.size() - 2; lNumberNumbers++)
+    {
+        for (unsigned int i = 0; i < lValues.size() - lNumberNumbers; i++)
+        {
+            uint64_t lSum = 0;
+            for (unsigned int j = i; j < i + lNumberNumbers; j++)
+            {
+                lSum += lValues[j];
+            }
+
+            if (lSum == pValueToSearch)
+            {
+                uint64_t lMin = lValues[i];
+                uint64_t lMax = lValues[i];
+
+                for (unsigned int j = i; j < i + lNumberNumbers; j++)
+                {
+                    if (lValues[j] > lMax)
+                    {
+                        lMax = lValues[j];
+                    }
+
+                    if (lValues[j] < lMin)
+                    {
+                        lMin = lValues[j];
+                    }
+                }
+                return lMax + lMin;
+            }
+        }
+    }
+
+    return 0;
+}
+
 int main(int pArgc, char** pArgv) 
 {
     cout << "Day 9\n";
     
-    if (pArgc < 2)
+    if (pArgc < 3)
     {
-        cout << "Missing input file name\n";
+        cout << "Missing input file name or number to check\n";
         return EXIT_FAILURE;
     }
+
+    unsigned int lNumbers = stoul(pArgv[2]);
     
     ifstream lInputFile(pArgv[1], std::ifstream::in);
 
@@ -75,7 +129,13 @@ int main(int pArgc, char** pArgv)
         lValues.emplace_back(stoull(lTemp));
     }
 
-    cout << "Part 1 : the invalid value is " << findInvalidValue(lValues, 25) << endl;
+    cout << "There is " << lValues.size() << " values\n";
+
+    uint64_t lPart1 = findInvalidValue(lValues, lNumbers);
+    cout << "Part 1 : the invalid value is : " << lPart1 << endl;
+
+    uint64_t lPart2 = resultFindRange(lValues, lPart1);
+    cout << "Part 2 : the sum is           : " << lPart2 << endl;
 
     return EXIT_SUCCESS;
 }
