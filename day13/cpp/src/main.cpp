56ba35575813b52f65c293a614c6f8995c7b3918
Marc Chapdelaine <marc.chapdelaine@gmail.com>
2020-12-18 10:25:54 -0500

    Day 13 : part 2 brute force does not work
    Need to be more cleaver with the solution

--- day13/cpp/src/main.cpp
+++ day13/cpp/src/main.cpp
@@ -12,6 +12,11 @@ unsigned int part1(unsigned int pTimeStamp, const vector<unsigned int>& pValues)
     unsigned int lIdMin = pTimeStamp;
     unsigned int lDifferenceMin = pTimeStamp;
 
+    if (pValues.size() == 0)
+    {
+        return 0;
+    }
+
     for (const auto lValue : pValues)
     {
         unsigned int lDiv = pTimeStamp / lValue;
@@ -30,6 +35,62 @@ unsigned int part1(unsigned int pTimeStamp, const vector<unsigned int>& pValues)
     }
 
     return lIdMin * lDifferenceMin;
+}
+
+// Brute force part 2. Works with example, not with dataset
+uint64_t part2(const vector<pair<unsigned int, unsigned int>>& pValues)
+{
+    uint64_t lTimeStamp = 0;
+    const unsigned int lNumberValues = pValues.size();
+
+    if (lNumberValues == 0)
+    {
+        return 0;
+    }
+
+    // Initialize all timestamps to 0
+    uint64_t lCurrentTimeStamps[lNumberValues] = { 0 };
+
+    // There is not too many values, cache, will be faster than using vector
+    unsigned int lOffsetsCache[lNumberValues];
+    unsigned int lValuesCache[lNumberValues];
+    for (unsigned int i = 0; i < lNumberValues; i++)
+    {
+        lValuesCache[i] = pValues[i].first;
+        lOffsetsCache[i] = pValues[i].second;
+    }
+
+    bool lFound = false;
+
+    uint64_t lLoopNumber = 1;
+
+    while (!lFound)
+    {
+        lTimeStamp = lValuesCache[0] * lLoopNumber;
+
+        lFound = true;
+
+        for (unsigned i = 1; i < lNumberValues; i++)
+        {
+            if (lCurrentTimeStamps[i] < lTimeStamp)
+            {
+                uint64_t lDiv = lTimeStamp / lValuesCache[i];
+                lDiv++;
+
+                lCurrentTimeStamps[i] = lValuesCache[i] * lDiv;
+            }
+
+            if (lCurrentTimeStamps[i] - lOffsetsCache[i] != lTimeStamp)
+            {
+                lFound = false;
+                break;
+            }
+        }
+
+        lLoopNumber++;
+    }
+
+    return lTimeStamp;
 }
 
 int main(int pArgc, char** pArgv) 
@@ -53,6 +114,8 @@ int main(int pArgc, char** pArgv)
     string lTimestamp;
     string lIdsString;
     vector<unsigned int> lIds;
+
+    vector<pair<unsigned int, unsigned int>> lPart2Values;
     
     getline(lInputFile, lTimestamp);
     getline(lInputFile, lIdsString);
@@ -60,16 +123,22 @@ int main(int pArgc, char** pArgv)
     stringstream lIdsStream(lIdsString);
 
     string lTemp;
-    for (unsigned int i; getline(lIdsStream, lTemp, ',');)
+    for (unsigned int i = 0; getline(lIdsStream, lTemp, ',');i++)
     {
         if (lTemp != "x")
         {
             lIds.emplace_back(stoul(lTemp));
+            lPart2Values.emplace_back(stoul(lTemp), i);
+            cout << "Got value " << stoul(lTemp) << " with offset " << i << endl;
         }
     }
 
-    unsigned int lPart1 = part1(stoul(lTimestamp), lIds);
-    cout << "Part 1 is : " << lPart1 << endl;
+
+    // unsigned int lPart1 = part1(stoul(lTimestamp), lIds);
+    // cout << "Part 1 is : " << lPart1 << endl;
+
+    uint64_t lPart2 = part2(lPart2Values);
+    cout << "Part 2 is : " << lPart2 << endl;
 
     return EXIT_SUCCESS;
 }
